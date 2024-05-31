e4ffa5380eeb5635f80018c6e42b2059629aafd9
Niklas Heim <heim.niklas@gmail.com>
2024-05-31 16:13:54 +0200

    identify wrong operations as inaccurate

--- test/localize.jl
+++ test/localize.jl
@@ -3,17 +3,20 @@ using IntervalArithmetic
 using OptiFloat
 using OptiFloat: all_subexpressions, local_error
 
-#expr = :(a + b * (c + a))
-expr = :(a + b)
-intervals = Dict(
-    :a => interval(-1e10,1e10),
-    :b => interval(-1e10,1e10),
-)
-
-#expr = :((1 / (x+1) - (2/x)) + (1/x+1))
-expr = :(1 / (x+1) - (2/x))
+expr = :((1 / (x+1) - (2/x)) + (1/x+1))
+#expr = :(1 / (x+1) - (2/x))
 x = Float16(1e2)
-f(x) = (1/(x-1) - 2/x) #+ (1/x+1)
+f(x) = (1/(x-1) - 2/x) + (1/x+1)
+
+# hmm, seems to identify the wrong operation as error prone
+# also why is there 1/x in all_subexpressions
+Dict(e => local_error(e,(:x,x)) for e in all_subexpressions(expr))
+
+abs(evaluate_exact(f,x) - f(x))
+
+
+
+#########################################################################
 
 expr = :(x^y / (x^y + 2))
 f(x,y) = x^y / (x^y + 2)
@@ -29,24 +32,19 @@ intervals = Dict(
     :y => interval(T(0.5),T(7.0)),
 )
 
-function sample(x::Interval{T}, batchsize::Int) where T
-    # TODO: sample random bits in the interval in stead of uniform dist
-    # TODO: make sure we can sample from (-Inf,Inf)
-    (a,b) = bounds(x)
-    rand(T, batchsize) .* (b-a) .+ a
-end
-function sample(x::Dict{Symbol,<:Interval}, batchsize::Int)
-    dict = Dict(k=>sample(i,batchsize) for (k,i) in x)
-    (zip(keys(x), p) for p in zip(values(dict)...))
-end
-
+# function sample(x::Interval{T}, batchsize::Int) where T
+#     # TODO: sample random bits in the interval in stead of uniform dist
+#     # TODO: make sure we can sample from (-Inf,Inf)
+#     (a,b) = bounds(x)
+#     rand(T, batchsize) .* (b-a) .+ a
+# end
+# function sample(x::Dict{Symbol,<:Interval}, batchsize::Int)
+#     dict = Dict(k=>sample(i,batchsize) for (k,i) in x)
+#     (zip(keys(x), p) for p in zip(values(dict)...))
+# end
 # function local_error(expr, intervals, batchsize=100)
 #     points = sample(intervals, batchsize)
 #     map(points) do point
 #         local_error(expr, point...)
 #     end
 # end
-
-
-# does not return errors that sum to evaluate_exact(f,x) - f(x)
-map(e->local_error(e,(:x,x)), all_subexpressions(expr))
