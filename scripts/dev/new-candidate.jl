f431e5b05b65b499935e37f84e68a11d492c87e4
Niklas Heim <heim.niklas@gmail.com>
2024-07-11 15:16:20 +0200

    fix candidate construction

--- scripts/dev/new-candidate.jl
+++ scripts/dev/new-candidate.jl
@@ -2,49 +2,63 @@ using DynamicExpressions
 using Metatheory
 using Metatheory.Rewriters
 using OptiFloat
+using OptiFloat: simplify
 
 struct CandidateRewriter{F}
     rw::F
 end
-
-
-rule = @rule a b (a-b) --> (a^2-b^2)/(a+b)
-
-original = :((-1x - sqrt(x^2 - 1)) / (log(x)))
-original = :(-x - sqrt(x^2 - 1))
-original = :(-x - sqrt(x^2))
 
 function (cr::CandidateRewriter)(expr)
     x = cr.rw(expr)
     if isnothing(x)
         expr
     else
-        Expr(:meta, :candidate, expr, operation(x), arguments(x)...)
+        Expr(:candidate, expr, operation(x), arguments(x)...)
     end
 end
 
+
+_candidates(expr, theory) = [expr]
+function _candidates(expr::Expr, theory)
+    if head(expr)==:candidate
+        orig = expr.args[1]
+        op = expr.args[2]
+        args = expr.args[3:end]
+        argss = [_candidates(arg, theory) for arg in args]
+
+        # only simplify children of current node
+        argss = map(argss) do args
+            map(a -> simplify(a,theory), args)
+        end
+
+        [orig; vec([Expr(:call, op, args...) for args in Iterators.product(argss...)])]
+    else
+        argss = [_candidates(arg, theory) for arg in arguments(expr)]
+        argss = Iterators.product(argss...)
+        [maketerm(typeof(expr), head(expr), [operation(expr); args...], nothing) for args in argss] |> vec
+    end
+end
+function candidates(expr, theory)
+    cs = _candidates(expr, theory)
+
+    # very first element contains original expr with Expr(:meta, :candidate, ...)s.
+    # remove it. keep only unique new exprs
+    unique(cs[2:end])
+end
+
+
+
+rule = @rule a b (a-b) --> (a^2-b^2)/(a+b)
+
+original = :((neg(1x) - sqrt(x^2 - 1)) / (log(x)))
+#original = :(-x - sqrt(x^2))
+#original = :(-x - sqrt(x^2 - 1))
+#original = :(neg(x) - sqrt(x^2 - 1))
 cr = CandidateRewriter(rule)
 y = cr(original)
 y = Postwalk(cr)(original)
 
 
-candidates(expr, theory) = [expr]
-function candidates(expr::Expr, theory)
-    if head(expr)==:meta && expr.args[1] == :candidate
-        orig = expr.args[2]
-        op = expr.args[3]
-        args = expr.args[4:end]
-        argss = [candidates(arg, theory) for arg in args]
-        @info "before" argss
-        argss = map(a -> OptiFloat.simplify(a,theory), argss)
-        @info "after" argss
-        [[orig]; vec([Expr(:call, op, args...) for args in Iterators.product(argss...)])]
-    else
-        argss = [candidates(arg, theory) for arg in arguments(expr)]
-        argss = Iterators.product(argss...)
-        [maketerm(typeof(expr), head(expr), [operation(expr); args...], nothing) for args in argss] |> vec
-    end
-end
 
 #candidates(:(1+1))
 candidates(y, OptiFloat.SIMPLIFY_THEORY)
