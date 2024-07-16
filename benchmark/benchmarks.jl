3c66b736b9a53abf85eb8f90a92dba5ba4a0eb42
Niklas Heim <heim.niklas@gmail.com>
2024-07-16 13:09:56 +0200

    Julia 1.11

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,5 +1,8 @@
 using Pkg
-Pkg.status()
+Pkg.update()
+Pkg.add(url = "https://github.com/JuliaSymbolics/Metatheory.jl", rev="ale/3.0")
+Pkg.add(url = "https://github.com/nmheim/DynamicExpressions.jl", rev="nh/early-exit")
+Pkg.dev(path = "..")
 
 using BenchmarkTools
 using DynamicExpressions: Node, parse_expression
@@ -16,3 +19,5 @@ xs = logsample(e1, 10000)
 SUITE["evaluate_approx"] = @benchmarkable evaluate_approx($e1, $xs)
 SUITE["evaluate_exact"] = @benchmarkable evaluate_exact($e1, $xs)
 SUITE["biterror"] = @benchmarkable biterror($e1, $e1, $xs)
+
+[ Info:     Creating temporary environment at /var/folders/qc/0bylx5y131z9pggm5w86jgnm0000gn/T/jl_j3y688.
