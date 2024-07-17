eb18c957b6fcc661da767dc2752f593047595237
Niklas Heim <heim.niklas@gmail.com>
2024-07-17 12:09:17 +0200

    update benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,13 +3,16 @@ using DynamicExpressions: Node, parse_expression
 using OptiFloat: evaluate_approx, evaluate_exact, biterror, logsample
 const SUITE = BenchmarkGroup()
 
-SUITE["ParametricSpeciesDictUnion"] = BenchmarkGroup()
-
 T = Float16
-kws = (; binary_operators=[-, +, /, ^, *], unary_operators=[sqrt], node_type=Node{T}, variable_names=["x"])
+kws = (;
+    binary_operators=[-, +, /, ^, *],
+    unary_operators=[sqrt],
+    node_type=Node{T},
+    variable_names=["x"],
+)
 expr = parse_expression(:(sqrt(x + 1) - sqrt(x)); kws...)
-xs = logsample(e1, 10000)
+xs = logsample(expr, T, 1, 10000)
 
-SUITE["evaluate_approx"] = @benchmarkable evaluate_approx($e1, $xs)
-SUITE["evaluate_exact"] = @benchmarkable evaluate_exact($e1, $xs)
-SUITE["biterror"] = @benchmarkable biterror($e1, $e1, $xs)
+SUITE["evaluate_approx"] = @benchmarkable evaluate_approx($expr, $xs)
+SUITE["evaluate_exact"] = @benchmarkable evaluate_exact($expr, $xs)
+SUITE["biterror"] = @benchmarkable biterror($expr, $expr, $xs)
